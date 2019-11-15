0442b60e55def9d2784900bbcb44714ace4218a3
cfowler650 <cfowler650@gmail.com>
2019-11-15 13:09:10 -0600

    Updates

---
+++ precache-manifest.a2879c6d9cb298d4022bc155ac5748b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dd28acda425d1438c8963a94c07eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41830128df0db4f42fc",
+    "url": "/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "877cd67d9c2c73667a10",
+    "url": "/static/css/main.1b5afb3b.chunk.css"
+  },
+  {
+    "revision": "d41830128df0db4f42fc",
+    "url": "/static/js/2.ee7591f9.chunk.js"
+  },
+  {
+    "revision": "877cd67d9c2c73667a10",
+    "url": "/static/js/main.f6cf03dc.chunk.js"
+  },
+  {
+    "revision": "8968dc9ab2eef81157eb",
+    "url": "/static/js/runtime-main.81f9d18f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
